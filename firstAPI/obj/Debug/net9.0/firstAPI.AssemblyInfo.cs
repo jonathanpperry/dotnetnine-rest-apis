a56496738f1fec82a4a0ab05193030532017c360
Jon Perry <jpper@umich.edu>
2025-06-03 06:29:41 -0400

    Specify first Book model

--- firstAPI/obj/Debug/net9.0/firstAPI.AssemblyInfo.cs
+++ firstAPI/obj/Debug/net9.0/firstAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e347d075da3eac16d2d48696c70fd7ed0cd167a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
