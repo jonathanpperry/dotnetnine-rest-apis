a3c7293994fdd85435c73d3231f41b1c652ab519
Jon Perry <jpper@umich.edu>
2025-07-09 00:11:44 -0400

    Implemented a seed of book data

--- firstAPI/Migrations/20250709040958_book data added.Designer.cs
+++ firstAPI/Migrations/20250709040958_book data added.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using firstAPI.Data;
 
@@ -10,9 +11,11 @@ using firstAPI.Data;
 namespace firstAPI.Migrations
 {
     [DbContext(typeof(FirstAPIContext))]
-    partial class FirstAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20250709040958_book data added")]
+    partial class bookdataadded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
